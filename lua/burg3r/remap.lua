1d74aa67b35410579fd63dbe15a4ceb41bb6712e
about14sheep <aburger91@gmail.com>
2024-04-28 15:09:58 -0500

    adds tabout

--- lua/burg3r/remap.lua
+++ lua/burg3r/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ","
-vim.keymap.set('n', '<tab>', vim.cmd.Ex, { desc = "open file explorer" })
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = "open file explorer" })
 vim.keymap.set('n', '<leader>n', ':set relativenumber! <cr>', { desc = "toggle standard line numbers" })
 vim.keymap.set('n', '<leader>w', ':wincmd v <cr>', { desc = "split window" })
 vim.keymap.set('n', '<leader>a', ':wincmd h <cr>', { desc = "move cursor to left window" })
