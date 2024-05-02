071319b5925e4a2e4e307d54f8838a2a0f00ffd0
about14sheep <aburger91@gmail.com>
2024-05-02 18:09:08 -0500

    faster buffers

--- lua/burg3r/remap.lua
+++ lua/burg3r/remap.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = ","
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = "open file explorer" })
+vim.keymap.set('n', '<leader>x', vim.cmd.Ex, { desc = "open file explorer" })
 vim.keymap.set('n', '<leader>n', ':set relativenumber! <cr>', { desc = "toggle standard line numbers" })
 vim.keymap.set('n', '<leader>w', ':wincmd v <cr>', { desc = "split window" })
 vim.keymap.set('n', '<leader>a', ':wincmd h <cr>', { desc = "move cursor to left window" })
 vim.keymap.set('n', '<leader>d', ':wincmd l <cr>', { desc = "move cursor to right window" })
-
+vim.keymap.set('n', '<leader>q', ':bprev <cr>', { desc = "move to previous buffer" })
+vim.keymap.set('n', '<leader>e', ':bnext <cr>', { desc = "move to next buffer" })
